8286b69e8988ee6ec448896058c058168718ed59
Zykoss <74079755+LamZykoss@users.noreply.github.com>
2025-03-20 17:40:13 -0300

    Add files via upload

---
+++ Unidad 1/Semana 2/ejp1.py
@@ -0,0 +1,11 @@
+print("Bienvenid@")
+print("----------")
+
+vnombre = input("Dime tu nombre: ")
+vapellido = input("Ahora tu apellido: ")
+vrut = input("Ingresa tu rut: ")
+vdirec = input("Ingresa tu dirección: ")
+correo = input("Por último dime tu correo: ")
+
+print(f"\n\n¡Hola {vnombre} {vapellido}! estos son tus datos:")
+print(f"Tu rut es: {vrut}\nVives en: {vdirec}\nY tu correo electrónico es: {correo}")
